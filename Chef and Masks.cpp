0754cf533e7200d756fb25ff5b4fa7998c9ad4c3
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-28 21:53:28 +0530

    Create Chef and Masks.cpp

---
+++ Chef and Masks.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;A
+	    int dm=x*100;
+	    int cm=y*10;
+	    if(dm>=cm){
+	        cout<<"Cloth"<<"\n";
+	    }
+	    else{
+	        cout<<"Disposable"<<"\n";
+	    }
+	}
+	return 0;
+}
