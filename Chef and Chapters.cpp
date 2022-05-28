cce6ec39dd8a6c9cdae9384d354ec9793736774c
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-28 11:24:11 +0530

    Create Chef and Chapters.cpp

---
+++ Chef and Chapters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int a,b,c;
+	while(t--){
+	    cin>>a>>b>>c;
+	    cout<<(a*b*c)<<"\n";
+	}
+	return 0;
+}
