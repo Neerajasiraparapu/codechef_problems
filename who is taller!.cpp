324b955ccd9a994081899942857764c123efa9a4
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-28 10:04:00 +0530

    Create who is taller!.cpp

---
+++ who is taller!.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int A,B;
+	while(t--){
+	    cin>>A>>B;
+	    if(A>B){
+	    cout<<"A"<<"\n";
+	    }
+	    else if(A<B)
+	    {
+	    cout<<"B"<<"\n";
+	    }
+	}
+	return 0;
+}
