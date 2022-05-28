3b75f91f5cc1a4723421855bb2c17fc550044282
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-28 11:56:11 +0530

    Create Apples and Oranges.cpp

---
+++ Apples and Oranges.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int a,b;
+	cin>>a>>b;
+	int c=((1*a)+(1*b));
+	if(c<=n){
+	    cout<<"Yes"<<"\n";
+	}
+	else
+	{
+	    cout<<"No";
+	}
+	return 0;
+}
