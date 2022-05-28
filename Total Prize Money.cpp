6a9153e9ee85898dd6872e61fcbfa68ceccbc898
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-28 22:40:43 +0530

    Create Total Prize Money.cpp

---
+++ Total Prize Money.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    cout<<((x*10)+(y*90))<<"\n";
+	}
+	return 0;
+}
