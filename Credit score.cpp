d5de74c25df6aa6148a608324623590a3ea34307
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-05-29 10:52:09 +0530

    Create Credit score.cpp

---
+++ Credit score.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n>=750){
+	    cout<<"YES"<<"\n";
+	}
+	else{
+	    cout<<"NO";
+	}
+	return 0;
+}
