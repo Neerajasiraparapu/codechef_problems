102fb467a4f5f291ed9d2e6354871c7c58730ced
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-06-02 20:11:03 +0530

    Create Battery Low.cpp

---
+++ Battery Low.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x<=15){
+	        cout<<"Yes"<<"\n";
+	    }
+	    else{
+	        cout<<"No"<<"\n";
+	    }
+	}
+	return 0;
+}
