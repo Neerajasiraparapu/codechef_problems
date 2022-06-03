1dd316ed6bfc9967c05d36426555d3a9e0246189
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-06-03 11:51:50 +0530

    Create Increase IQ.cpp

---
+++ Increase IQ.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	int y=x+7;
+	if(y>170){
+	    cout<<"Yes";
+	}
+	else{
+	    cout<<"No";
+	}
+	return 0;
+}
