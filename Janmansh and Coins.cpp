c2e45da9953956cc9f7d170f161445dcec816606
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-06-06 20:07:16 +0530

    Create Janmansh and Coins.cpp

---
+++ Janmansh and Coins.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    cout<<((x*10)+(y*5))<<"\n";
+	}
+	
+	return 0;
+}
