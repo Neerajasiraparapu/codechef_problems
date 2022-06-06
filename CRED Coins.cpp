03b5b2849f055debd4af9ca14b42150156b5260a
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-06-06 20:55:14 +0530

    Create CRED Coins.cpp

---
+++ CRED Coins.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	     cin>>x>>y;
+	     int a=x*y;
+	     int r=a/100;
+	     cout<<r<<"\n";
+	}
+	return 0;
+}
