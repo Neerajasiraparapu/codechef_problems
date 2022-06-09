16881cf04431999e9fc1f90cf0fdfda4304d68b3
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-06-09 17:22:44 +0530

    Create Bath in Winters.cpp

---
+++ Bath in Winters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    int p=2*y;
+	    cout<<x/p<<"\n";
+	}
+	return 0;
+}
