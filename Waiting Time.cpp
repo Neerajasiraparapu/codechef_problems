970403c1e54ffc3b3751b581b483728eba3b3efb
Neerajasiraparapu <84018439+Neerajasiraparapu@users.noreply.github.com>
2022-06-16 19:55:15 +0530

    Create Waiting Time.cpp

---
+++ Waiting Time.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,k,x;
+	cin>>t;
+	while(t--){
+	    cin>>k>>x;
+	    cout<<((7*k)-x)<<"\n";
+	}
+	return 0;
+}
